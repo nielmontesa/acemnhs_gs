56bd6b34dfc585835a846f2e323d81e843e6c26f
nielmontesa <nirumontesa@gmail.com>
2024-09-21 11:00:20 +0800

    Formatting

--- src/pages/user_authentication/sign-up.php
+++ src/pages/user_authentication/sign-up.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
     // Retrieve input values
     $role = $_POST['role'];
     $username = trim($_POST['username']);
-    $password = trim($_POST['password']); 
+    $password = trim($_POST['password']);
 
     // Adjust query based on role
     if ($role == 'admin') {
@@ -54,7 +54,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
 </head>
 
 <body class="h-screen flex justify-center items-center">
-    <div class="bg-gray-100 border-2 border-gray-200 rounded-xl py-16 px-12 m-6 flex flex-col gap-4 align-center justify-center">
+    <div
+        class="bg-gray-100 border-2 border-gray-200 rounded-xl py-16 px-12 m-6 flex flex-col gap-4 align-center justify-center">
         <div class="w-full rounded flex justify-center">
             <img src="../../assets/acemnhs_logo.png" />
         </div>
@@ -69,11 +70,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
                             Admin
                         </label>
                         <label>
-                            <input type="radio" name="role" value="teachers" class="btn bg-[rgba(0,0,0,0.02)]" required />
+                            <input type="radio" name="role" value="teachers" class="btn bg-[rgba(0,0,0,0.02)]"
+                                required />
                             Teacher
                         </label>
                         <label>
-                            <input type="radio" name="role" value="parents" class="btn bg-[rgba(0,0,0,0.02)]" required />
+                            <input type="radio" name="role" value="parents" class="btn bg-[rgba(0,0,0,0.02)]"
+                                required />
                             Parent
                         </label>
                     </div>
@@ -81,17 +84,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
                 <div class="flex flex-col gap-2">
                     <label for="username">
                         <span class="text-xs pb-4 pl-2 text-[rgba(0,0,0,0.5)] font-medium">Username</span>
-                        <input class="input-block input" placeholder="Please enter your username." name="username" type="text" required />
+                        <input class="input-block input" placeholder="Please enter your username." name="username"
+                            type="text" required />
                     </label>
                     <label for="Password">
                         <span class="text-xs pb-4 pl-2 text-[rgba(0,0,0,0.5)] font-medium">Password</span>
-                        <input class="input-block input" placeholder="Please enter your password." name="password" type="password" required />
+                        <input class="input-block input" placeholder="Please enter your password." name="password"
+                            type="password" required />
                     </label>
                     <button class="btn btn-primary btn-block mt-2" name="submit" type="submit">Sign-up</button>
                 </div>
             </div>
         </form>
-        <p class="text-sm text-center"> Already have an account? <a href="../../index.php" class="link text-sm text-[rgba(0,0,0,0.8)] underline">Log-in</a></p>
+        <p class="text-sm text-center"> Already have an account? <a href="../../index.php"
+                class="link text-sm text-[rgba(0,0,0,0.8)] underline">Log-in</a></p>
     </div>
 </body>
+
 </html>

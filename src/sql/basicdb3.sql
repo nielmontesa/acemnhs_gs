40b7efd4756966305c42f4c24380986683b2402c
nielmontesa <nirumontesa@gmail.com>
2024-10-01 19:53:58 +0800

    Updated table.

--- src/sql/basicdb3.sql
+++ src/sql/basicdb3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 01, 2024 at 01:46 PM
+-- Generation Time: Oct 01, 2024 at 01:53 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,193 @@ SET time_zone = "+00:00";
 --
 -- Database: `esguerradb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `username`, `password`) VALUES
+(1, 'nmontesa', 'bruh'),
+(2, 'raymart', '$2y$10$kBjc2MNVXwhUcFyHi6jPDexKDcz40jKzV2XRMq48nXcFO.7wGynGa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `attendance_id` int(11) NOT NULL,
+  `student_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `department_id` int(11) NOT NULL,
+  `department_name` varchar(255) DEFAULT NULL,
+  `teacher_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gradesheet`
+--
+
+CREATE TABLE `gradesheet` (
+  `gradesheet_id` int(11) NOT NULL,
+  `student_id` int(11) DEFAULT NULL,
+  `section_id` int(11) DEFAULT NULL,
+  `department_id` int(11) DEFAULT NULL,
+  `written_work_id` int(11) DEFAULT NULL,
+  `performance_task_id` int(11) DEFAULT NULL,
+  `quaterassessment_id` int(11) DEFAULT NULL,
+  `attendance_id` int(11) DEFAULT NULL,
+  `quarter_status` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parents`
+--
+
+CREATE TABLE `parents` (
+  `parent_id` int(11) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `contact.no` int(11) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `student_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `parents`
+--
+
+INSERT INTO `parents` (`parent_id`, `username`, `password`, `first_name`, `last_name`, `contact.no`, `email`, `student_id`) VALUES
+(1, 'nmontesa', 'bruh', NULL, NULL, NULL, NULL, NULL),
+(2, 'nielmontesa', 'burat', NULL, NULL, NULL, NULL, NULL),
+(3, 'bruh', 'bruh', NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `performancetask`
+--
+
+CREATE TABLE `performancetask` (
+  `performance_task_id` int(11) NOT NULL,
+  `student_id` int(11) DEFAULT NULL,
+  `department_id` int(11) DEFAULT NULL,
+  `gradesheet_id` int(11) DEFAULT NULL,
+  `score` int(11) DEFAULT NULL,
+  `max_score` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `performancetask_final`
+--
+
+CREATE TABLE `performancetask_final` (
+  `pfinal_id` int(11) NOT NULL,
+  `student_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quarterassessment`
+--
+
+CREATE TABLE `quarterassessment` (
+  `quaterassessment_id` int(11) NOT NULL,
+  `student_id` int(11) DEFAULT NULL,
+  `department_id` int(11) DEFAULT NULL,
+  `gradesheet_id` int(11) DEFAULT NULL,
+  `score` int(11) DEFAULT NULL,
+  `max_score` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quarterassessment_final`
+--
+
+CREATE TABLE `quarterassessment_final` (
+  `qafinal_id` int(11) NOT NULL,
+  `student_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report_card`
+--
+
+CREATE TABLE `report_card` (
+  `report_id` int(11) NOT NULL,
+  `student_id` int(11) DEFAULT NULL,
+  `gradesheet_id` int(11) DEFAULT NULL,
+  `final_grades` decimal(3,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `section`
+--
+
+CREATE TABLE `section` (
+  `section_id` int(11) NOT NULL,
+  `grade_level` int(11) DEFAULT NULL,
+  `section_name` varchar(255) DEFAULT NULL,
+  `is_archived` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `section`
+--
+
+INSERT INTO `section` (`section_id`, `grade_level`, `section_name`, `is_archived`) VALUES
+(6, 7, 'Rizal', 1),
+(7, 8, 'Bonifacio', 1),
+(8, 9, 'Luna', 1),
+(9, 10, 'Lapu-lapu', 1),
+(10, 10, 'Lapu-lapu', 1),
+(11, 7, 'Gomez', 1),
+(12, 7, 'Rizal', 1),
+(13, 7, 'Gomez', 1),
+(14, 7, 'Rizal', 1),
+(15, 8, 'Rizal', 1),
+(16, 7, 'Gomez', 1),
+(17, 9, 'Lapu-lapu', 1),
+(18, 10, 'Bonifacio', 1),
+(19, 7, 'Rizal', 0),
+(20, 7, 'Bonifacio', 0),
+(21, 8, 'Rizal', 0);
 
 -- --------------------------------------------------------
 
@@ -48,9 +235,146 @@ INSERT INTO `students` (`student_id`, `first_name`, `last_name`, `section_ID`, `
 (1335789490, 'Alexander', 'Berdera', 19, 'Inactive', 'aberdera@gmail.com', 'Female', 0, 0),
 (2021102643, 'Niel Ivan', 'Montesa', 12, 'Active', 'nirumontesa@gmail.com', 'Male', 1, 0);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `teacher_id` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `department_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `teachers`
+--
+
+INSERT INTO `teachers` (`teacher_id`, `username`, `password`, `first_name`, `last_name`, `email`, `department_id`) VALUES
+(1, 'aberdera', 'bruh', NULL, NULL, NULL, NULL),
+(2, 'nmontesa', 'bruh', NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `writtenwork`
+--
+
+CREATE TABLE `writtenwork` (
+  `written_work_id` int(11) NOT NULL,
+  `student_id` int(11) DEFAULT NULL,
+  `department_id` int(11) DEFAULT NULL,
+  `gradesheet_id` int(11) DEFAULT NULL,
+  `score` int(11) DEFAULT NULL,
+  `max_score` int(11) DEFAULT NULL,
+  `final_score` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `writtenwork_final`
+--
+
+CREATE TABLE `writtenwork_final` (
+  `wwfinal_id` int(11) NOT NULL,
+  `student_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`attendance_id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`department_id`),
+  ADD KEY `fk_teacher` (`teacher_id`) USING BTREE;
+
+--
+-- Indexes for table `gradesheet`
+--
+ALTER TABLE `gradesheet`
+  ADD PRIMARY KEY (`gradesheet_id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `section_id` (`section_id`),
+  ADD KEY `department_id` (`department_id`),
+  ADD KEY `written_work_id` (`written_work_id`),
+  ADD KEY `performance_task_id` (`performance_task_id`),
+  ADD KEY `quaterassessment_id` (`quaterassessment_id`),
+  ADD KEY `attendance_id` (`attendance_id`);
+
+--
+-- Indexes for table `parents`
+--
+ALTER TABLE `parents`
+  ADD PRIMARY KEY (`parent_id`),
+  ADD KEY `fk_students` (`student_id`);
+
+--
+-- Indexes for table `performancetask`
+--
+ALTER TABLE `performancetask`
+  ADD PRIMARY KEY (`performance_task_id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `department_id` (`department_id`),
+  ADD KEY `gradesheet_id` (`gradesheet_id`);
+
+--
+-- Indexes for table `performancetask_final`
+--
+ALTER TABLE `performancetask_final`
+  ADD PRIMARY KEY (`pfinal_id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `quarterassessment`
+--
+ALTER TABLE `quarterassessment`
+  ADD PRIMARY KEY (`quaterassessment_id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `department_id` (`department_id`),
+  ADD KEY `gradesheet_id` (`gradesheet_id`);
+
+--
+-- Indexes for table `quarterassessment_final`
+--
+ALTER TABLE `quarterassessment_final`
+  ADD PRIMARY KEY (`qafinal_id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `report_card`
+--
+ALTER TABLE `report_card`
+  ADD PRIMARY KEY (`report_id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `gradesheet_id` (`gradesheet_id`);
+
+--
+-- Indexes for table `section`
+--
+ALTER TABLE `section`
+  ADD PRIMARY KEY (`section_id`);
 
 --
 -- Indexes for table `students`
@@ -58,6 +382,127 @@ INSERT INTO `students` (`student_id`, `first_name`, `last_name`, `section_ID`, `
 ALTER TABLE `students`
   ADD PRIMARY KEY (`student_id`),
   ADD KEY `fk_sections` (`section_ID`);
+
+--
+-- Indexes for table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`teacher_id`),
+  ADD KEY `fk_departments` (`department_id`);
+
+--
+-- Indexes for table `writtenwork`
+--
+ALTER TABLE `writtenwork`
+  ADD PRIMARY KEY (`written_work_id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `department_id` (`department_id`),
+  ADD KEY `gradesheet_id` (`gradesheet_id`);
+
+--
+-- Indexes for table `writtenwork_final`
+--
+ALTER TABLE `writtenwork_final`
+  ADD PRIMARY KEY (`wwfinal_id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gradesheet`
+--
+ALTER TABLE `gradesheet`
+  MODIFY `gradesheet_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `parents`
+--
+ALTER TABLE `parents`
+  MODIFY `parent_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `performancetask`
+--
+ALTER TABLE `performancetask`
+  MODIFY `performance_task_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `performancetask_final`
+--
+ALTER TABLE `performancetask_final`
+  MODIFY `pfinal_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `quarterassessment`
+--
+ALTER TABLE `quarterassessment`
+  MODIFY `quaterassessment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `quarterassessment_final`
+--
+ALTER TABLE `quarterassessment_final`
+  MODIFY `qafinal_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report_card`
+--
+ALTER TABLE `report_card`
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `section`
+--
+ALTER TABLE `section`
+  MODIFY `section_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `writtenwork`
+--
+ALTER TABLE `writtenwork`
+  MODIFY `written_work_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `writtenwork_final`
+--
+ALTER TABLE `writtenwork_final`
+  MODIFY `wwfinal_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `parents`
+--
+ALTER TABLE `parents`
+  ADD CONSTRAINT `fk_students` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
